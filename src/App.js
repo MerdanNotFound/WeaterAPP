48aad6c249a102add53ee2774ad219168805932b
Merdan <wisew.tube@gmail.com>
2024-12-12 14:01:59 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Search from "./components/Search";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+};
+
+export default App;
